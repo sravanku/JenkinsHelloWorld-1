f77ab2761de694030fbb915135fbc52e287032cf
sravanku <46219046+sravanku@users.noreply.github.com>
2020-02-19 17:08:18 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
 	public static void main(String[] args){
-		System.out.println("-----***** Poll SCM *****-----");
+		System.out.println("-----***** Poll SCM Update *****-----");
 	}
 }
