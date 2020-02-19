3c2ea83049c97b56ea48515788ee17109b1dbd36
sravanku <46219046+sravanku@users.noreply.github.com>
2020-02-19 17:34:29 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
 	public static void main(String[] args){
-		System.out.println("-----***** Poll SCM Update *****-----");
+		System.out.println("-----***** Webhook Example *****-----");
 	}
 }
