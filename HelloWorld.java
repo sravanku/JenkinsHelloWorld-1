10623f43dde7e1dad3aa5c055de34253081d82c5
sravanku <46219046+sravanku@users.noreply.github.com>
2020-02-19 17:36:20 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
 	public static void main(String[] args){
-		System.out.println("-----***** Webhook Example *****-----");
+		System.out.println("-----***** Webhook Working *****-----");
 	}
 }
